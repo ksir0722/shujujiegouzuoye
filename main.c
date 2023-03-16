67ddd1e76a6a20ab4b7b859afc5343cd496779d7
KueFone <fengmoxiaofeng@gmail.com>
2023-03-16 22:55:09 +0800

    1

---
+++ main.c
@@ -0,0 +1,18 @@
+#define MAXSIZE 10000
+#define ERROR 0
+#define OVERFLOW -2
+
+typedef struct
+{
+    char id[20];
+    char name[10];
+    char sex;
+    int age;
+    char class;
+}student;
+
+void InitList_Sd(student *L){
+    L->elem =(int *)malloc(MAXSIZE *sizeof(int));
+    if(!L->elem) exit(OVERFLOW);
+    L->length=0;
+}
